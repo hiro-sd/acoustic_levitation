853b1e49c4830a8dd40ad6b42fb1e29af7fa90c2
Hiroto Yoshida <jum6308@gmail.com>
2025-05-29 19:08:09 +0900

    リファクタ

--- proj_hiraisan.py
+++ proj_hiraisan.py
@@ -98,28 +98,28 @@ if __name__ == "__main__":
                 center = autd.center() + np.array([x, y, z]) # 円軌道の中心座標を更新
 
                 # 円軌道上に焦点を配置するための時空間変調 (鉛直方向への移動のみならこれでよい) (単焦点)
-                stm = FociSTM(
-                    foci = (
-                        center + radius * np.array([np.cos(theta), np.sin(theta), 0])
-                        for theta in (2.0 * np.pi * i / point_num for i in range(point_num))
-                        ),
-                    config = 100 * Hz, # 100Hzで更新(1秒間に円周上を100周する)
-                ).into_nearest() # point_num = 40kHz/Nを満たすNが存在しない場合、エラーになる
-
-                # # 斜めの円軌道上に焦点を配置するための時空間変調
-                # tilt_angle = np.pi / 6  # 30度の傾き
-                # rotation_matrix = np.array([
-                #     [1, 0, 0],
-                #     [0, np.cos(tilt_angle), -np.sin(tilt_angle)],
-                #     [0, np.sin(tilt_angle), np.cos(tilt_angle)]
-                # ])
                 # stm = FociSTM(
                 #     foci = (
-                #         center + rotation_matrix @ (radius * np.array([np.cos(theta), np.sin(theta), 0]))
+                #         center + radius * np.array([np.cos(theta), np.sin(theta), 0])
                 #         for theta in (2.0 * np.pi * i / point_num for i in range(point_num))
                 #         ),
-                #     config = 100 * Hz,
-                # ).into_nearest()
+                #     config = 100 * Hz, # 100Hzで更新(1秒間に円周上を100周する)
+                # ).into_nearest() # point_num = 40kHz/Nを満たすNが存在しない場合、エラーになる
+
+                # 斜めの円軌道上に焦点を配置するための時空間変調
+                tilt_angle = np.pi / 6  # 30度の傾き
+                rotation_matrix = np.array([
+                    [1, 0, 0],
+                    [0, np.cos(tilt_angle), -np.sin(tilt_angle)],
+                    [0, np.sin(tilt_angle), np.cos(tilt_angle)]
+                ])
+                stm = FociSTM(
+                    foci = (
+                        center + rotation_matrix @ (radius * np.array([np.cos(theta), np.sin(theta), 0]))
+                        for theta in (2.0 * np.pi * i / point_num for i in range(point_num))
+                        ),
+                    config = 100 * Hz,
+                ).into_nearest()
 
                 # stm = build_stm_alternate(center)
 
