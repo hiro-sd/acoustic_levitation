df64accb1cc86670318ca9f1e48832978ac26536
Hiroto Yoshida <jum6308@gmail.com>
2025-06-23 16:30:58 +0900

    リファクタリング

--- balloon_levitation/balloon_levitation_vertical.py
+++ balloon_levitation/balloon_levitation_vertical.py
@@ -39,10 +39,10 @@ if __name__ == "__main__":
 
         autd.send(Silencer())
 
-        m = Static(intensity=0xFF) # 振幅変調を行わず、常に同じ振幅を出力する
+        m = Static(intensity=int(0xFF)) # 振幅変調を行わず、常に同じ振幅を出力する
 
-        point_num = 7 # 円周上の点の数
-        radius = 45.0 # 円軌道の半径
+        point_num = 8 # 円周上の点の数
+        radius = 24.0 # 円軌道の半径
         x, y, z = 0.0, 0.0, 400.0 # x,y,z座標の初期値
         x_min, x_max = -100.0, 100.0 # x座標の最小値と最大値
         y_min, y_max = -150.0, 150.0 # y座標の最小値と最大値
